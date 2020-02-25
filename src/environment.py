edd177c5218b22a7ab44fbec7bf432b4c5b01383
Viktor <vik@quadraticlabs.com>
2020-02-25 21:43:17 +0200

    correct work

--- src/environment.py
+++ src/environment.py
@@ -4,20 +4,16 @@ from gym import spaces, Env
 import gym
 from src.utils import *
 from src.utils_plot import *
-import random
+
 
 import os
-
-
+from collections import deque
 import numpy as np
 import matplotlib.pyplot as plt
 
 MAX_STEPS = 1000
 MAX_VALUES = 1000
 MAX_LOSS = 10000
-
-
-
 
 class WhirlwindEnvironment(Env):
     metadata = {'render.modes': ['human']}
@@ -33,49 +29,59 @@ class WhirlwindEnvironment(Env):
         """
         super(WhirlwindEnvironment, self).__init__()
 
-        self.n_whirlwind = config_input_data['n_whirlwind']
-        self.distribution_params = config_input_data['distribution_params']
-        self.points_z = generate_random_dots(
-            config_input_data['min_value'],
-            config_input_data['max_value'],
-            self.n_whirlwind)
+        self.config_input_data = config_input_data
         self.config_dict = config_dict
-        self.betta = config_input_data['betta_0']
-        if not self.config_dict['use_the_random_noise']:
-            self.alfa = config_input_data['alfa_0']
-        else:
-            self.alfa = 0
+        self.init_func()
 
-        # self.iteration = config_dict['iteration']
-
-
-        if self.config_dict['use_the_random_noise']:
-            self.ksi = generate_noise_dots(self.n_whirlwind, config_input_data['distribution_params'])
-        else:
-            self.ksi = np.zeros(self.points_z.shape)
 
         self.action_space = spaces.Discrete(3) #spaces.Box(
             #low=0, high=1, shape=(1, ), dtype=np.int)
 
         # Prices contains the OHCL values for the last five prices
         # + betta + distance change
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(self.points_z.size + 1 + 3,), dtype=np.float16)
-            #spaces.Box(low=0, high=np.inf, shape=self.points_z.shape, dtype=np.float16),
-
-
+        self.observation_space = spaces.Box(low=-np.inf,
+                                            high=np.inf,
+                                            shape=(self.config_dict['memory_size'],
+                                                   self.current_position.size
+                                                   + 1 + self.config_input_data['n_whirlwind']),
+                                            dtype=np.float16)
         self.force_mag = 0.001
 
-        self.II_pev = circulation_moment(self.points_z)
-        self.current_betta = self.betta
-        self.current_alfa = self.alfa
-        self.current_position = self.points_z
+    def init_func(self):
+
+        self.current_position = generate_random_dots(
+            self.config_input_data['min_value'],
+            self.config_input_data['max_value'],
+            self.config_input_data['n_whirlwind'])
+        if self.config_dict['use_the_random_noise']:
+            self.current_alfa = config_input_data['alfa_0']
+            self.ksi = generate_noise_dots(self.config_input_data['n_whirlwind'],
+                                           self.config_input_data['distribution_params'])
+        else:
+            self.ksi = np.zeros(self.current_position.shape)
+            self.current_alfa = 0
+
+        self.current_betta = self.config_input_data['betta_0']
+        self.II_pev = circulation_moment(self.current_position)
+        # self.current_alfa = self.alfa
         self.betta_list = []
         self.loss = self.II_pev
         self.current_step = 0
 
+        self.memory_state = deque([self.get_one_observation(self.current_position)
+                                  for i in range(self.config_dict['memory_size'])], self.config_dict['memory_size'])
 
 
 
+    def get_one_observation(self, new_position):
+
+        distance = [calculate_the_measure(new_p, old_p)  for (new_p, old_p) in zip(new_position,self.current_position)]
+        one_obs =  np.concatenate([
+                new_position.flatten(),
+                [self.current_betta],
+                distance
+            ])
+        return one_obs
 
     def _next_observation(self):
         # Get the stock data points for the last 5 days and scale to between 0-1
@@ -88,42 +94,41 @@ class WhirlwindEnvironment(Env):
 
         #print("new_position", new_position)
         #print("[self.current_betta]", [self.current_betta])
-        distance = [calculate_the_measure(new_p, old_p)  for (new_p, old_p) in zip(new_position,self.current_position)]
-        obs =  np.concatenate(new_position)
-        obs =  np.concatenate([obs, [self.current_betta], distance])
+
+        #print()
+        obs = self.get_one_observation(new_position)
+
+        self.memory_state.append(obs)
         self.current_position = np.stack(new_position, axis=0)
         # print("next _obs self.current_position", self.current_position)
         #print("obs", obs)
-        return obs
+        return np.array(self.memory_state)#obs
 
     def _take_action(self, action):
         # Set the current price to a random price within the time step
         if action == 0:
-            pass
-        elif action == 1:
             self.current_betta += self.force_mag
-
-        elif action == 2:
-            new_betta = self.current_betta - self.force_mag
+        elif action == 1:
+            self.current_betta -= self.current_betta - self.force_mag
             # i = 1
             # while new_betta < 0:
             #     new_betta = self.current_betta - self.force_mag / i
             #     i += 1
 
-            self.current_betta = new_betta
-
-        # self.current_betta += self.force_mag if action == 1 else -self.force_mag
+        elif action == 2:
+            pass
 
     def info_dictionary(self, action):
 
         main_measure_dict = {
             'action': action,
             'points': self.current_position,
+            'memory_state': self.memory_state,
             'center': circulation_center(self.current_position),
             'moment': circulation_moment(self.current_position),
             'dispersion': circulation_dispersion(self.current_position),
-            'alfa': self.alfa,
-            'betta': self.betta,
+            'alfa': self.current_alfa,
+            'betta': self.current_betta,
             #'gamma': self.gamma,
             'alfa_loss': self.loss
               }
@@ -147,14 +152,15 @@ class WhirlwindEnvironment(Env):
             print("action: ", action)
             II_next = circulation_moment(self.current_position)
 
-            self.loss = np.abs(self.II_pev - (II_next + np.sum([
-                save_div(1 - betta, betta)  for betta in self.betta_list
-                            ])))
+            self.loss = np.log(
+                    np.abs(self.II_pev - II_next) + np.sum([
+                            save_div(1 - betta, betta)  for betta in self.betta_list
+                            ]))
             self.II_pev = II_next
 
             reward = - self.loss
 
-            if self.loss >= 10000:
+            if self.loss >= 1000:
 
                 obs = self._next_observation()
                 done = True
@@ -179,14 +185,7 @@ class WhirlwindEnvironment(Env):
 
     def reset(self):
         # Reset the state of the environment to an initial state
-
-        self.current_betta = self.betta
-        self.current_alfa = self.alfa
-        self.current_position = self.points_z
-        self.betta_list = []
-        self.loss = self.II_pev
-        self.current_step = 0
-
+        self.init_func()
 
         return self._next_observation()
 
@@ -199,9 +198,10 @@ class WhirlwindEnvironment(Env):
 
 
 config_dict = {
-    'find_use_best_alfa': False,
+    #'find_use_best_alfa': False,
     'use_second_interaction': False,
-    'use_the_random_noise': False
+    'use_the_random_noise': False,
+    'memory_size': 5
 }
 
 config_input_data = {
@@ -286,7 +286,7 @@ mean_reward, n_steps = evaluate_policy(model, model.get_env(), n_eval_episodes=1
 
 
 # Instantiate the agent
-model = DQN('MlpPolicy', env, learning_rate=1e-3, prioritized_replay=True, verbose=1)
+model = DQN('MlpPolicy', env, learning_rate=1e-2, prioritized_replay=True, verbose=1)
 # Train the agent
 model.learn(total_timesteps=int(2e5))
 
@@ -294,50 +294,10 @@ model.learn(total_timesteps=int(2e5))
 mean_reward, n_steps = evaluate_policy(model, model.get_env(), n_eval_episodes=100)
 
 
-
-
-from stable_baselines.ddpg.policies import LnMlpPolicy
-from stable_baselines.bench import Monitor
-from stable_baselines.results_plotter import load_results, ts2xy
-from stable_baselines import DDPG
-from stable_baselines.ddpg import AdaptiveParamNoiseSpec
-from stable_baselines import results_plotter
-
-best_mean_reward, n_steps = -np.inf, 0
-
-def callback(_locals, _globals):
-    """
-    Callback called at each step (for DQN an others) or after n steps (see ACER or PPO2)
-    :param _locals: (dict)
-    :param _globals: (dict)
-    """
-    global n_steps, best_mean_reward
-    # Print stats every 1000 calls
-    if (n_steps + 1) % 1000 == 0:
-        # Evaluate policy training performance
-        x, y = ts2xy(load_results(log_dir), 'timesteps')
-        if len(x) > 0:
-            mean_reward = np.mean(y[-100:])
-            print(x[-1], 'timesteps')
-            print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(best_mean_reward, mean_reward))
-
-            # New best model, you could save the agent here
-            if mean_reward > best_mean_reward:
-                best_mean_reward = mean_reward
-                # Example for saving best model
-                print("Saving new best model")
-                _locals['self'].save(log_dir + 'best_model.pkl')
-    n_steps += 1
-    return True
-
-# Create log dir
-log_dir = "tmp/"
-os.makedirs(log_dir, exist_ok=True)
-
 # Create and wrap the environment
 #env = gym.make('LunarLanderContinuous-v2')
 
-
+"""
 env = DummyVecEnv([lambda: WhirlwindEnvironment(config_dict, config_input_data)])
 
 env = Monitor(env, log_dir, allow_early_resets=True)
@@ -356,13 +316,14 @@ plt.show()
 from stable_baselines import ACKTR
 model = ACKTR(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=25000)
-
-
+"""
+MAX_STEPS = 10000
 obs = env.reset()
 aggregate_info = []
-for i in range(200):
+for i in range(MAX_STEPS):
     action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
+    if done: break
     env.render()
     aggregate_info.append(info)
 
@@ -370,8 +331,9 @@ for i in range(200):
 
 main_measure_dict = {k: [dic[k] for dic in  aggregate_info] for k in aggregate_info[0]}
 
+np.unique(main_measure_dict['betta'])
 
-for type_graph in list(main_measure_dict.keys())[2:]:
+for type_graph in list(main_measure_dict.keys())[3:]:
     try:
         plot_graph = main_measure_dict[type_graph]
         if plot_graph != []:
@@ -380,7 +342,8 @@ for type_graph in list(main_measure_dict.keys())[2:]:
         continue
 
 
+
 len(main_measure_dict['points'])
 
-sample_points = [main_measure_dict['points'][i] for i in range(0,200)]
+sample_points = [main_measure_dict['points'][i] for i in range(0,len(main_measure_dict['points']), 25)]
 plot_the_whirlwind(sample_points)
